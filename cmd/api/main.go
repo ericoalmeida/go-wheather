303ffca175e2dc73686937f85d2200403acfb702
Erico Almeida <erico.silva@banqi.com>
2025-05-11 16:44:08 -0400

    feat: add test endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 
 	"github.com/ericoalmeida/go-wheather/internal/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
-
 	fmt.Println(cfg.GeoapifyBaseUrl)
+
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	message := "Hello World"
+
+	json.NewEncoder(w).Encode(message)
 }
